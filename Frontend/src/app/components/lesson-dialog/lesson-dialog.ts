b4daab32ee311d33070b8125cec55543aff56547
Miriam <Hand1e5@mbjcomp.org.il>
2026-05-14 15:55:04 +0300

    building all the files for the frontend

---
+++ Frontend/src/app/components/lesson-dialog/lesson-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-dialog',
+  imports: [],
+  templateUrl: './lesson-dialog.html',
+  styleUrl: './lesson-dialog.css',
+})
+export class LessonDialog {
+
+}
